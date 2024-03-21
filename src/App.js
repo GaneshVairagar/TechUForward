043aabcdaab0697a0320dced7323d0f27fbbd7c5
GaneshVairagar <vairagarganesh7498@gmail.com>
2024-03-21 17:32:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import axios from 'axios';
+import './App.css';
+import Table from './Components/page2/Table';
+import Form from './Components/page1/Form';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData(); 
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/getAllFormData'); 
+      setData(response.data); 
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form />
+      <Table data={data} /> 
     </div>
   );
-}
+};
+
 
 export default App;
